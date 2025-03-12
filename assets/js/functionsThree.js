2a757e76b23bb960e346bbea0712cd5d6bb95c2e
jako6857 <82064889+jako6857@users.noreply.github.com>
2025-03-12 14:07:13 +0100

    opg 3

--- assets/js/functionsThree.js
+++ assets/js/functionsThree.js
@@ -1,17 +1,14 @@
-
-
-
 // her skal du skrive din anonyme funktion
-let myAnonFunc=
-
+let myAnonFunc = function () {
+  return "Hello World";
+};
 
 // her kaldes, eller invokes din anonyme funktion gennem en variabel
 console.log(myAnonFunc());
 
-
 // her skal du skrive din arrow funktion
-let myArrowFunc=
-
-
+let myArrowFunc = () => {
+  return "Hellow World";
+};
 // her kaldes, eller invokes din arrow funktion gennem en variabel
 console.log(myArrowFunc());
